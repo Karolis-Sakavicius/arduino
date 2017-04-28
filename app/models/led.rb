80c3147965cd0082dfce10c7c289307465d2d78f
Karolis-Sakavicius <karolis.sakavicius@synergium.eu>
2017-04-28 04:29:53 +0300

    Communication protocol changes
    * Add timeouts (prevent endless loops that might crash RPi)
    * Wipe previous buffer before reading response from Arduino

--- app/models/led.rb
+++ app/models/led.rb
@@ -1,12 +1,10 @@
 class LED
   def self.set(led, red, green, blue)
-    serialport = Serial.new '/dev/arduino'
-
-    serialport.write([led, red, green, blue].join(' ')+ "\n")
+    self.connection.write([led, red, green, blue].join(' ')+ "\n")
   end
 
   def self.reset(size)
-    serialport = Serial.new '/dev/arduino'
+    serialport = self.connection
 
     (size * size).times do |led|
       serialport.write((led + 1).to_s + ' 0 0 0' + "\n")
@@ -14,14 +12,19 @@ class LED
   end
 
   def self.current_state
-    serialport = Serial.new '/dev/arduino'
+    serialport = self.connection
 
     serialport.write("a\n")
     response = ''
 
     response_char = serialport.getbyte&.chr
 
+    timeout = Time.current + 10.seconds
+
     while response_char != "\n"
+      if Time.current >= timeout
+        raise 'Timeout reached.'
+      end
       response += response_char.to_s
       response_char = serialport.getbyte&.chr
     end
@@ -35,5 +38,13 @@ class LED
     end
 
     return false
+  end
+
+  def self.connection
+    # establish connection and wipe previous buffer
+    serialport = Serial.new '/dev/arduino'
+    serialport.read(64)
+
+    serialport
   end
 end
