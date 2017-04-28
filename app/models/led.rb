42f51c44fdd4b8e665157c7e6873f300ac5d1b61
Karolis-Sakavicius <karolis.sakavicius@synergium.eu>
2017-04-28 03:48:56 +0300

    Use /dev/arduino symlink instead of ttyACM0

--- app/models/led.rb
+++ app/models/led.rb
@@ -1,12 +1,12 @@
 class LED
   def self.set(led, red, green, blue)
-    serialport = Serial.new '/dev/ttyACM0'
+    serialport = Serial.new '/dev/arduino'
 
     serialport.write([led, red, green, blue].join(' ')+ "\n")
   end
 
   def self.reset(size)
-    serialport = Serial.new '/dev/ttyACM0'
+    serialport = Serial.new '/dev/arduino'
 
     (size * size).times do |led|
       serialport.write((led + 1).to_s + ' 0 0 0' + "\n")
