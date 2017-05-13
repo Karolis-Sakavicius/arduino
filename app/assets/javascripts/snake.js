ce081f604d2daa55f440864e4a972bce1491a3bd
Karolis-Sakavicius <karolis.sakavicius@synergium.eu>
2017-05-13 04:56:47 +0300

    Add snake controlling functionality

--- app/assets/javascripts/snake.js
+++ app/assets/javascripts/snake.js
@@ -3,7 +3,11 @@ $(document).ready(function() {
   head_direction = 1;
 
   arrayToId = function(posX, posY) {
-    return (posX * 9 + posY);
+    return (posX * 9 + posY + 1);
+  }
+
+  drawHead = function() {
+    $.patch('leds/' + arrayToId(snake_positions[snake_positions.length-1][0], snake_positions[snake_positions.length-1][1]), { 'red': 0, 'green': 0, 'blue': 255 });
   }
 
   $.patch('leds/1', { 'red': 0, 'green': 0, 'blue': 255 }, function() {
@@ -15,11 +19,42 @@ $(document).ready(function() {
   });
 
   $('.snake-right').click(function() {
-    $.patch('leds/' + arrayToId(snake_positions[0][0], snake_positions[0][1]), { 'red': 0, 'green': 0, 'blue': 0 }, function() {
-      $.each(snake_positions, function(index, value) {
-        snake_positions[index][1] = snake_positions[index][1] + 1;
+    if (snake_positions[snake_positions.length-1][1] + 1 < 9) {
+      $.patch('leds/' + arrayToId(snake_positions[0][0], snake_positions[0][1]), { 'red': 0, 'green': 0, 'blue': 0 }, function() {
+        snake_positions.shift();
+        snake_positions.push([snake_positions[snake_positions.length-1][0], snake_positions[snake_positions.length-1][1] + 1]);
+        drawHead();
       });
-      $.patch('leds/' + arrayToId(snake_positions[snake_positions.length-1][0], snake_positions[snake_positions.length-1][1]), { 'red': 0, 'green': 0, 'blue': 255 });
-    });
+    }
+  });
+
+  $('.snake-down').click(function() {
+    if (snake_positions[snake_positions.length-1][0] + 1 < 9) {
+      $.patch('leds/' + arrayToId(snake_positions[0][0], snake_positions[0][1]), { 'red': 0, 'green': 0, 'blue': 0 }, function() {
+        snake_positions.shift();
+        snake_positions.push([snake_positions[snake_positions.length-1][0] + 1, snake_positions[snake_positions.length-1][1]]);
+        drawHead();
+      });
+    }
+  });
+
+  $('.snake-up').click(function() {
+    if (snake_positions[snake_positions.length-1][0] - 1 >= 0) {
+      $.patch('leds/' + arrayToId(snake_positions[0][0], snake_positions[0][1]), { 'red': 0, 'green': 0, 'blue': 0 }, function() {
+        snake_positions.shift();
+        snake_positions.push([snake_positions[snake_positions.length-1][0] - 1, snake_positions[snake_positions.length-1][1]]);
+        drawHead();
+      });
+    }
+  });
+
+  $('.snake-left').click(function() {
+    if (snake_positions[snake_positions.length-1][1] - 1 >= 0) {
+      $.patch('leds/' + arrayToId(snake_positions[0][0], snake_positions[0][1]), { 'red': 0, 'green': 0, 'blue': 0 }, function() {
+        snake_positions.shift();
+        snake_positions.push([snake_positions[snake_positions.length-1][0], snake_positions[snake_positions.length-1][1] - 1]);
+        drawHead();
+      });
+    }
   });
 });
